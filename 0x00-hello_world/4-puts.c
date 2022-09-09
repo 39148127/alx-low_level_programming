0a56fbe7b724c5bf64004df97758421c4cb3f451
39148127 <kwambokalilian59@gmail.com>
2022-09-08 17:26:41 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
 Description: main-prints out a string to the standard output
-Return: it returns zero
+Return: zero if success
 */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 
 }
