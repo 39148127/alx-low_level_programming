5f3ce4cfa03b714af4055f3e186a17d5ff52b2c0
39148127 <kwambokalilian59@gmail.com>
2022-09-08 18:27:57 -0700

    Size of C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always ZERO if success
+ */
+int main(void)
+{
+printf("Size of a char: %ld byte(s)\n", sizeof(long unsigned int));
+printf("Size of an int: %ld byte(s)\n", sizeof(long unsigned int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long unsigned int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long unsigned int));
+printf("Size of a float: %ld byte(s)\n", sizeof(long unsigned int));
+return (0);
+}
