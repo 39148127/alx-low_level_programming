e14b46d9abae65c88c0e29bbc95713c545a6c779
39148127 <kwambokalilian59@gmail.com>
2022-09-14 15:49:44 -0700

    Hope

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
  */
 void print_alphabet_x10(void)
 {
-	int w;
-	char x;
-	w = 0;
+int w;
+char x;
+w = 0;
 
-	while (w <= 9)
-	{
-		for (x = 'a'; x <= 'z'; x++)
-		{
-			_putchar(x);
-		}
-		w++;
-	        _putchar('\n');
-	}
+while (w <= 9)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+w++;
+_putchar('\n');
+}
 }	
