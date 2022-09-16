7cafa63f4066c1d8b82fe15b77dc5f6baff29805
39148127 <kwambokalilian59@gmail.com>
2022-09-16 14:59:44 -0700

    Is super

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ * main- checks ofor uppercase character
+ *
+ *return - always 0
+ */
+int main(void)
+{
+	char c;
+	if (_issupper(c))
+		_putchar ("1);
+	else
+		_putchar ("0);
+return (0);
+}
