b340627fd0612032fc6e3d6a0e8052d09a20329c
39148127 <kwambokalilian59@gmail.com>
2022-09-16 18:46:56 -0700

    Anonymously yours

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ *print_numbers - print numbers
+ *
+ *Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+	for (x = '0'; x < '10'; x++)
+	{
+		_putchar(x);
+	        _putchar("\n");
+	}
+}
