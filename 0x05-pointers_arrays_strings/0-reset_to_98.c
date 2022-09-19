8213b9e3ef3691a3d0cce77c737985f9dcb74fbe
39148127 <kwambokalilian59@gmail.com>
2022-09-19 16:17:06 -0700

    Reset pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *reset_to_98(int *n) - takes a pointer as a parameter and updates it
+ * reset_to_98(int *n) - takes a pointer as a parameter and updates it
  *
- *
+ * @n: value of a pointer
  *
  */
 
