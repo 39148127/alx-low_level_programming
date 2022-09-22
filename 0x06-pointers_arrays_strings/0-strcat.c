a3bfd9959d005b899b6859cbbb47fe729bff2386
39148127 <kwambokalilian59@gmail.com>
2022-09-22 12:31:16 -0700

    srtcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: destination
+ *
+ *  @src: source
+ *
+ *  return: return dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int k = 0;
+
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
+	while(dest[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
+
